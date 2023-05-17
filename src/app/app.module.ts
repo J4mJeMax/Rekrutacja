7f0c6de33b8cf8a0e70bada754ac6a4b30e7f723
j4mjemax <H20-wdoa123>
2023-05-18 00:55:57 +0200

    created services and modularized clients

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClientsComponent } from './views/clients/clients.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { LayoutComponent } from './core/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientsComponent,
     NavbarComponent,
     LayoutComponent
   ],
